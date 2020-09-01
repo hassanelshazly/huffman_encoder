55a232d69b485cf152738219cd652f47223ff6ae
Hassan El-shazly <hassanelshazly2010@gmail.com>
2020-09-01 02:58:14 +0200

    Add more options

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,25 @@
  * forms is permitted as long as the files maintain this copyright.
  *
  *****************************************************************************/
- /**
+/**
   *
   * @author Hassan El-shazly
   * @date Last Edit Aug-2020
   *
   */
 #include <iostream>
+#include <string>
+#include <fstream>
 
 #include "huffman.h"
 
 using namespace std;
+// using experimental::filesystem;
 
 void print_inst();
+bool exists(const string &path);
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     string mood;
     string input;
@@ -34,56 +38,83 @@ int main(int argc, char** argv)
     mood = argv[1];
     input = argv[2];
 
+    if (mood != "-e" && mood != "-d" && mood != "--encode" && mood != "--decode")
+    {
+        cout << "exit_status(1): you must provide \"-e\" or \"-d\" argument !" << endl;
+        print_inst();
+        return 1;
+    }
+
+    if (!exists(input))
+    {
+        cout << "exit_status(2): couldn't open \"" << input << "\"" << endl;
+        return 2;
+    }
+
     if (argc == 3)
-        if(input != "huffman_out.txt")
+        if (input != "huffman_out.txt")
             output = "huffman_out.txt";
         else
             output = "huffman_output.txt";
     else
         output = argv[3];
 
-    if (mood != "-e" && mood != "-d" && mood != "--encode" && mood != "--decode")
+    if (input == output)
     {
-        cout << "exit_statue(1): you must provide \"-e\" or \"-d\" argument !" << endl;
-        print_inst();
-        return 1;
+        cout << "exit_status(5): you can't overwrite the input file" << endl;
+        return 5;
+    }
+
+    if (exists(output))
+    {
+        cout << "\"" << output << "\" already exists, the contents will be overwitten\n"
+             << "Do you want to continue [yes/no]: " << endl;
+        string response;
+        cin >> response;
+        if (response != "y" && response != "yes")
+        {
+            cout << "process terminated" << endl;
+            return 0;
+        }      
     }
 
     ifstream input_file(input, std::ios::in | std::ios::binary);
-    if (!input_file.is_open()) 
+    if (!input_file.is_open())
     {
-        cout << "exit_statue(2): couldn't open \"" << input << "\"" << endl;
+        cout << "exit_status(2): couldn't open \"" << input << "\"" << endl;
         return 2;
     }
 
     ofstream output_file(output, std::ios::out | std::ios::binary);
-    if (!output_file.is_open()) 
+    if (!output_file.is_open())
     {
-        cout << "exit_statue(2): couldn't open \"" << output << "\"" << endl;
+        cout << "exit_status(2): couldn't open \"" << output << "\"" << endl;
         return 2;
     }
 
-    if (mood ==  "-e" || mood == "--encode")
+    if (mood == "-e" || mood == "--encode")
     {
         try
         {
             float ratio = huff.encode(input_file, output_file);
-            cout << "Compression ratio: " << ratio*100 << "%" << endl;
+            cout << "Compression ratio: " << ratio * 100 << "%" << endl;
         }
-        catch (const char * ex) {
-            cout << "exit_statue(3): Empty file \"" << input << "\"" << endl;
+        catch (const char *ex)
+        {
+            cout << "exit_status(3): Empty file \"" << input << "\"" << endl;
         }
     }
-    else if (mood ==  "-d" || mood == "--decode")
+    else if (mood == "-d" || mood == "--decode")
     {
         try
         {
             huff.decode(input_file, output_file);
         }
-        catch (const char * ex)
+        catch (const char *ex)
         {
-            cout << "exit_statue(4): \"" << input << "\"" << " is not vaild for decoding\n"
-                 << "                 the file must be encoded with the smae program " << endl;
+            cout << "exit_status(4): \"" << input << "\""
+                 << " is not vaild for decoding\n"
+                 << "                 the file must be encoded with the same program " << endl;
         }
     }
 
@@ -91,7 +122,8 @@ int main(int argc, char** argv)
     output_file.close();
 }
 
-void print_inst() {
+void print_inst()
+{
     cout << "huffman encoder\n\n";
     cout << "Usage: ./huffman.out [-e] [--encode] [-d] [--decode] <input_file> [output_file]\n\n";
     cout << "if the <output_file> is not provided the default will be \"huffman_out.txt\"\n\n";
@@ -99,4 +131,10 @@ void print_inst() {
     cout << "  -d --decode  decode the <input_file> an store in the <output_file>\n";
 
     cout << endl;
+}
+
+bool exists(const string &path)
+{
+    std::ifstream f(path.c_str());
+    return f.good();
 }
